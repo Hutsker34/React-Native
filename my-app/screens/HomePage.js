6d34be26760063c0675eea1024cb877d1568c51e
viacheslav <77201329+viacheslav34@users.noreply.github.com>
2023-10-28 18:37:33 +0300

    add track component styles

--- my-app/screens/HomePage.js
+++ my-app/screens/HomePage.js
@@ -1,36 +1,12 @@
 import { View,  StyleSheet , TouchableOpacity, Modal,TextInput, Button, Alert, Text,Pressable, ImageBackground} from 'react-native';
-import { Audio } from 'expo-av';
 import React, { useState, useEffect } from 'react';
 import Track from '../components/Track'
 
-const HomePage =  (props) => {
-    
-
-
-
-
-     useEffect(() => {
-         // Создаем асинхронную функцию внутри useEffect
-         const loadAndPlayAudio = async () => {
-             const soundObject = new Audio.Sound();
-             try {
-                 // Загружаем аудио
-                 await soundObject.loadAsync(require('../audio/test2.mp3'));
-                 await soundObject.setVolumeAsync(0.7, 0)
-                 // Воспроизводим аудио
-                 await soundObject.playAsync();
-             } catch (error) {
-                 console.log("Ошибка при загрузке или воспроизведении аудио:", error);
-             }
-         };
-
-         // Вызываем созданную асинхронную функцию
-         loadAndPlayAudio
-     }, []);  // Пустой массив зависимостей означает, что этот эффект будет выполнен только один раз при монтировании компонента
+const HomePage =  () => {
 
     return (
         <View style={styles.HomePageWrap}>
-           
+           <Track/>
         </View>
     );
 };
@@ -39,6 +15,9 @@ const styles = StyleSheet.create({
             width:'100%',
             height: '100%',
             backgroundColor: '#01CD98',
+            flex: 1,
+            alignItems: 'center',
+            paddingTop: 40,
         }
 
 })
