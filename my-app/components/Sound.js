a1b1bb568bd8937454e9d9756aca24bbd0397923
Polina Shneider <beatrixlinkoln@gmail.com>
2024-01-13 18:31:14 +0100

    bug fixes

--- my-app/components/Sound.js
+++ my-app/components/Sound.js
@@ -4,118 +4,95 @@ import { Image, View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import pauseIcon from '../assets/pause.png';
 import playIcon from '../assets/play.png';
 
-const Sound = ({playNext, currentSound, isActive, setCurrentSoundIndex, index, name }) => {
+const Sound = ({playNext, sound, playbackStatus, isActive, setCurrentSoundIndex, index, name }) => {
     const ref = useRef(null);
-
-
-    
-    const [sound, setSound] = useState(null);
-    const [playbackStatus, setPlaybackStatus] = useState();
+    // Текущий объект звука Audio.Sound
+//    const [sound, setSound] = useState(null);
+    // Объект статуса, связанный с sound
+//    const [playbackStatus, setPlaybackStatus] = useState(null);
+    // Ширина прогресс-бара
     const [trackWidth, setTarckwidth] = useState(0)
+    // Количество проигранный секунд
     const [trackMillis, setTrackMillis] = useState(0)
+    const [isPlaying, setIsPlaying] = useState(false);
 
-    const onPlaybackStatusUpdate = (status) => {
-      setPlaybackStatus(status);
-      setTrackMillis(status.positionMillis)
-    };
+//    const onPlaybackStatusUpdate = (status) => {
+//      setPlaybackStatus(status);
+////      setTrackMillis(status.positionMillis)
+//    };
+    
+//    const getProgress = () => {
+//      if (playbackStatus) {
+//        const progress =
+//          (playbackStatus.positionMillis / playbackStatus.durationMillis) * 100;
+//        return `${Math.round(progress)}%`;
+//      }
+//      return '0%';
+//    };
 
+//    function setPlayPosition(event){
+//      let position = playbackStatus ? (playbackStatus.durationMillis * event.nativeEvent.locationX) / trackWidth : 0
+//
+//      sound.playFromPositionAsync(position)
+//      setTrackMillis(position)
+//    }
 
     
-    
-    const getProgress = () => {
-      if (playbackStatus) {
-        const progress =
-          (playbackStatus.positionMillis / playbackStatus.durationMillis) * 100;
-        return `${Math.round(progress)}%`;
-      }
-      return '0%';
-    };
-
-    function setPlayPosition(event){
-      
-      let position = playbackStatus ? (playbackStatus.durationMillis * event.nativeEvent.locationX) / trackWidth : 0
-      
-      sound.playFromPositionAsync(position)
-      setTrackMillis(position)
-      
-    }
-
-    
-    useEffect(() => {
-      
-      if(playbackStatus && playbackStatus.didJustFinish){
-        playNext()
-        // sound.stopAsync()
-        
-      }
-    }, [playbackStatus])
+//    useEffect(() => {
+//      if(playbackStatus && playbackStatus.didJustFinish){
+//        // Должен триггерить обновление sound и playBackStatus
+//        console.log('it doesnt happen spontaneously')
+//        playNext();
+//        // sound.stopAsync()
+//      }
+//    }, [playbackStatus])
 
 
-
-    useEffect(() => {
-     
-      if (!sound) {
-        return
-        
-      }
-      sound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
-      
-
-    }, [sound]);
+//
+//    useEffect(() => {
+//      if (!sound ) {
+//        return
+//      }
+//
+//      const loadSound = async () => {
+//        const status = await sound.getStatusAsync()
+//        // Должно триггерить обновление playBackStatus
+//        // Когда меняется sound? Не триггерит ли он по цепочке currentSoundIndex через playNext
+//        if(status.isLoaded){
+////          sound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+//        }
+//      }
+//
+//      loadSound();
+//    }, [sound]);
 
 
 
-    useEffect(() => {
-      
-        let isCancelled = false;
-        const updeteAudio = async () => {
-          if(currentSound){
-            const status = await currentSound.getStatusAsync()
-            if(status.isLoaded){
-              setSound(currentSound)
-            }
-          }
-        }
-        updeteAudio()
-       
-        // const loadAudio = async () => {
-        //     const soundObject = new Audio.Sound();
-        //     try {
-        //         await soundObject.loadAsync(path);
-        //         await soundObject.setVolumeAsync(0.7);
-        //         if (!isCancelled) {
-        //             setSound(soundObject);
-        //         }
-        //     } catch (error) {
-        //         console.error("Error loading or playing audio:", error);
-        //     }
-        // };
-
-        // loadAudio();
-
-        return () => {
-            isCancelled = true;
-            sound?.unloadAsync();
-        };
-        
-    }, [currentSound]);
-
-    useEffect(() => {
-        
-        
-    }, [isActive]);
+//    useEffect(() => {
+////        let isCancelled = false;
+//        const updateAudio = async () => {
+//          if(sound){
+//            const status = await currentSound.getStatusAsync()
+//            if(status.isLoaded){
+//              // Какая у него связанность с sound, по ссылке или нет
+//              // Не срабатывает ли этот код при нажатии на паузу
+//              setSound(currentSound)
+//            }
+//          }
+//        }
+//        updateAudio()
+//
+////        return () => {
+////            isCancelled = true;
+////            sound?.unloadAsync();
+////        };
+//
+//    }, [sound]);
 
 
-    
 
     function getPlayIcon(){
-      
-      if(!playbackStatus ){
-        return playIcon
-      }
-    
-     
-      if(playbackStatus.isPlaying && isActive){
+      if(isPlaying && isActive){
         return pauseIcon
       }else{
         return playIcon
@@ -123,28 +100,52 @@ const Sound = ({playNext, currentSound, isActive, setCurrentSoundIndex, index, n
     }
     
     const handlePlayPause = async () => {
-        
+        // Нужна ли проверка на isActive?
+
         setCurrentSoundIndex(index);
-        if(!playbackStatus || !isActive ){
-          return 
-        }
-        
-        if(playbackStatus.isPlaying){
+
+        console.log('handlePlayPause', isActive);
+        if(isPlaying){
           await sound.pauseAsync();
         }else{
            await sound.replayAsync();
         }
-        console.log(index,isActive,playbackStatus)
+//        console.log(index,isActive,playbackStatus)
     };
+
+//    useEffect(() => {
+//      const updateStatus = async () => {
+//
+//        const playbackStatus = await sound.getStatusAsync();
+//
+//        sound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+//      }
+//
+//      if (!sound) {
+//          return
+//      }
+//      updateStatus();
+//
+//      return () => sound.setOnPlaybackStatusUpdate(null);
+//    }, [sound])
+
+    useEffect(() => {
+            console.log('**********************************')
+            console.log(playbackStatus, 'inside update status', isActive, index)
+            if (playbackStatus) {
+                setIsPlaying(playbackStatus.isPlaying);
+            }
+            console.log('**********************************')
+    }, [playbackStatus])
 
     return (
     <View style={styles.track__wrap}>
       <Text  style={styles.track__wrapName}>{name}</Text>
-      {isActive &&
+      {false &&
         <View onLayout={(event) => {
           const {width} = event.nativeEvent.layout;
           setTarckwidth({width}.width)
-          
+
           }}
           ref={ref} onTouchStart={(event) => setPlayPosition(event)} style={styles.progressBarContainer}>
         <View style={[styles.progressBar, { width: getProgress() }]} />
--- my-app/screens/HomePage.js
+++ my-app/screens/HomePage.js
@@ -19,21 +19,20 @@ const names = sounds.keys().map((key) => {
 })
 
 const HomePage =  () => {
-
- 
     const [currentSoundIndex , setCurrentSoundIndex] = useState(-1)
-    const [ lastSound, setLastSound] = useState(null)
-    const [currentQueueSound, setCurrentQueueSound] = useState(null)
-   
+    // Текущий трек (объект Audio.Sound)
+    const [currentSound, setCurrentSound] = useState(null)
+    // Его статус
+    const [playbackStatus, setPlaybackStatus] = useState(null);
 
     async function playSoundsSequentially(sounds) {
         for (const soundFile of sounds) {
           const { sound } = await Audio.Sound.createAsync(soundFile);
-
-          setCurrentQueueSound(sound)
+          setCurrentSound(sound)
+          // Если еще не инициализировали
           if(currentSoundIndex < 0){
             return
-          }else{
+          } else {
             await sound.playAsync();
             await new Promise(resolve => {
               sound.setOnPlaybackStatusUpdate(playbackStatus => {
@@ -43,64 +42,74 @@ const HomePage =  () => {
               });
             });
           }
-          
-         
-    
           await sound.unloadAsync();
         }
       }
 
 
+    // Загружаем все звуки
     const soundFiles = sounds.keys().map(sounds);
-    
-    
 
-    useEffect( () => {
+    const onPlaybackStatusUpdate = (status) => {
+      setPlaybackStatus(status);
+//      setTrackMillis(status.positionMillis)
+    };
+
+    useEffect(() => {
+      const updateStatus = async () => {
+        const playbackStatus = await currentSound.getStatusAsync();
+
+        currentSound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+      }
+
+      if (!currentSound) {
+          return
+      }
+      updateStatus();
+
+      return () => currentSound.setOnPlaybackStatusUpdate(null);
+    }, [currentSound])
+
+    // Срабатывает на изменение номера трека
+    useEffect(() => {
+        // Новая очередь треков
         const currentSoundsQueue = soundFiles.slice(currentSoundIndex)
+        // Очистка ресурсов
         async function unloadSound(){
-            if(currentQueueSound){
-                await currentQueueSound.unloadAsync();
+            if(currentSound){
+                await currentSound.unloadAsync();
             }
         }
         unloadSound();
+        // Запускаем новую очередь
         playSoundsSequentially(currentSoundsQueue)
-        
-
     }, [currentSoundIndex])
 
     function changeCurrentSound(index){
        if(index !== currentSoundIndex){
-        
         setCurrentSoundIndex(index)
        }
-        
-        
     }
    
     function playNext(){
-        if(currentSoundIndex < soundFiles.length -1){
-            setCurrentSoundIndex(currentSoundIndex +1)
-            
+        if(currentSoundIndex < soundFiles.length - 1){
+            setCurrentSoundIndex(currentSoundIndex + 1)
         }
-       
     }
    
     
     return (
         <View style={styles.HomePageWrap}>
-             
              <FlatList
                 data={imageSources}
                 keyExtractor={(item, index) => index}
-                renderItem={({item , index}) => <Sound playNext={playNext}  currentSound={currentQueueSound}   isActive={currentSoundIndex == index} setCurrentSoundIndex={changeCurrentSound} currentSoundIndex={currentSoundIndex} name={names[index]}  index={index}  />}
+                renderItem={({item , index}) => <Sound playNext={playNext}  sound={currentSound} playbackStatus={playbackStatus} isActive={currentSoundIndex == index} setCurrentSoundIndex={changeCurrentSound} currentSoundIndex={currentSoundIndex} name={names[index]}  index={index}  />}
             />
-           
         </View>
     );
 };
 const styles = StyleSheet.create({
     HomePageWrap: {
-            
             width:'100%',
             height: '100%',
             backgroundColor: '#111',
